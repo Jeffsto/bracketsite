be3ee94a95198fe8ee08e4f8024874de1fbab2dc
Jeff S <jeffsto@gmail.com>
2023-02-03 15:01:23 -0500

    Creating initial settings for DB

--- bracketsite/settings.py
+++ bracketsite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bracketsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'web',
+        'USER': 'webadmin',
+        'PASSWORD' : '',
+        'HOST' : '10.10.10.10',
+        'PORT' : '3306',
     }
 }
 
