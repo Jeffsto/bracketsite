7734721c884e74c6d7e8bbb33d7314f4ec52a5c3
Jeff S <jeffsto@gmail.com>
2023-02-04 11:01:50 -0500

    First page that displays by data input in URL

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.users, name='users'),
+    path('users/', views.members, name='users'),
     path('users/details/<int:id>',views.details, name='details'),
 ]
