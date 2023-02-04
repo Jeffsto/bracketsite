09ab15c1d455296e6a76fb72d217587338a9237c
Jeff S <jeffsto@gmail.com>
2023-02-03 23:04:18 -0500

    First DB data to display

--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import users
 
 def members(request):
+    myusers = Member.objects.all().values()
     template = loader.get_template('users.html')
-    return HttpResponse(template.render())
+    context = {
+        'myusers':myusers,
+    }
+    return HttpResponse(template.render(context,request))
