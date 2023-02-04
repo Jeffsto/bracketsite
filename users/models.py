3cdad3bf41d8a167ad77a853e197e9eeedc84f0e
Jeff S <jeffsto@gmail.com>
2023-02-04 14:20:25 -0500

    Adding the custom model to the admin page

--- users/models.py
+++ users/models.py
@@ -7,3 +7,6 @@ class Member(models.Model):
   email = models.EmailField(max_length=255)
   phone = models.IntegerField(null=True)
   joined_date  = models.DateField(null=True)
+
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
