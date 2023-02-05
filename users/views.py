134a96f0ee97296500b67f2e02eccd98de7fbab1
Jeff S <jeffsto@gmail.com>
2023-02-04 23:57:07 -0500

    Adding the custom model to the admin page

--- users/views.py
+++ users/views.py
@@ -22,3 +22,10 @@ def details(request, id):
 def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
+
+def testing(request):
+  template = loader.get_template('testing.html')
+  context = {
+    'firstname': 'Linus',
+  }
+  return HttpResponse(template.render(context, request))
