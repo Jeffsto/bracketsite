9fa9dbb7162f8ef87bfeaa5d2c60f0a0abc80c6d
Jeff S <jeffsto@gmail.com>
2023-02-04 23:58:37 -0500

    Adding the custom model to the admin page

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('users/', views.members, name='users'),
     path('users/details/<int:id>',views.details, name='details'),
     path('',views.main,name='main')
-    path('testing',views.testing,name='testing')
+    path('users/testing',views.testing,name='testing'),
 ]
