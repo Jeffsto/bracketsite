919cae42c3eb8f0e4e7af04343dad34ce8a87941
Jeff S <jeffsto@gmail.com>
2023-02-19 10:27:50 -0500

    Working on login page

--- bracketsite/urls.py
+++ bracketsite/urls.py
@@ -19,6 +19,6 @@ from django.urls import include,path
 urlpatterns = [
     path('',include('users.urls')),
     path('admin/', admin.site.urls),
-    path('login/', include('django.contrib.auth.urls'))
+    path('login/', include('django.contrib.auth.urls')),
     path('login/', include('login.html')),
 ]
