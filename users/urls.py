3965114e9c16fa07a67e83caeb4a73dff71483ab
Jeff S <jeffsto@gmail.com>
2023-02-20 09:26:54 -0500

    Working on login page

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('users/', views.members, name='users'),
     path('users/details/<int:id>',views.details, name='details'),
-    path('',views.main,name='main'),
     path('users/testing',views.testing,name='testing'),
 ]
