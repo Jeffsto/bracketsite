c7194a03ea491226cc67e40e1f490a16985ba856
Jeff S <jeffsto@gmail.com>
2023-02-20 09:14:11 -0500

    Working on login page

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.template import loader
 
 # Create your views here.
 
