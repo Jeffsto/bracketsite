3cf720e3896f2d0f6ff45cb985fa15269c6f47ef
Jeff S <jeffsto@gmail.com>
2023-02-22 18:20:06 -0500

    Working on login page

--- login/views.py
+++ login/views.py
@@ -7,7 +7,20 @@ from django.http import HttpResponse
 # Create your views here.
 
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method == "POST":
+        def my_view(request):
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('main')
+            else:
+                # Return an 'invalid login' error message.
+                messages.succes(request, (f'There was an issue logging in'))
+                return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
 
 def main(request):
     template = loader.get_template('main.html')
